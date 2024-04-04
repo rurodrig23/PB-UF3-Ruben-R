3b36a0223c606efde8b31404888b51325a3e5a56
rurodrig23 <rubennrp37@gmail.com>
2024-04-04 08:36:57 +0200

    intento

--- canvi.py
+++ canvi.py
@@ -1,4 +1,9 @@
 import os
 
-def canviar_nom_fitxer():
-    ruta_fitxer = input("Introduïu la ruta del fitxer: ")
+ruta_fitxer = input("Introduïu la ruta del fitxer: ")
+
+pd = open("/home/alumne_1r/Escriptori/PB/PB-UF3-Ruben-R/canvi.py","r")
+
+os.listdir()
+
+print(pd)
