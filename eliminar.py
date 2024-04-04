9e6e6e9c1235e75f85faf8b3b23e07a55079f2b7
rurodrig23 <rubennrp37@gmail.com>
2024-04-04 09:42:46 +0200

    top down estructura

---
+++ eliminar.py
@@ -0,0 +1,17 @@
+import os
+
+
+'''
+1. Función que reciba la ruta de la carpeta como entrada.
+2. Ver si la ruta es válida y si la carpeta existe.
+3. Recorrer recursivamente todos los elementos dentro de la carpeta.
+4. Eliminar todos los elementos (archivos y carpetas) dentro de la carpeta.
+5. Repetir el proceso de eliminación para las subcarpetas de forma recursiva.
+6. Eliminar la carpeta original una vez que esté vacía.
+
+'''
+
+
+
+
+
